707600e7d850f83a60bdae9bbba6288840c0ebcf
Kailash1233 <kailash61203@gmail.com>
2024-12-29 22:16:35 +0530

    final commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // plugins: [sitemap({
+  //   hostname: "https://www.meiawards.in/", 
+  //   routes: [
+  //     "/",
+  //   ],
+  // }),],
+})
